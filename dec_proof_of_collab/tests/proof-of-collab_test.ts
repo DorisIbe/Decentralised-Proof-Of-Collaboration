4a4e9459d302d590c1aa0a0dd4ab1bf57eeb453e
DorisIbe <doris.ibe2024@gmail.com>
2025-01-30 06:32:23 +0100

    Add a test case to Ensure that contract initializes correctly with owner as admin

--- dec_proof_of_collab/tests/proof-of-collab_test.ts
+++ dec_proof_of_collab/tests/proof-of-collab_test.ts
@@ -3,24 +3,22 @@ import { Clarinet, Tx, Chain, Account, types } from 'https://deno.land/x/clarine
 import { assertEquals } from 'https://deno.land/std@0.90.0/testing/asserts.ts';
 
 Clarinet.test({
-    name: "Ensure that <...>",
-    async fn(chain: Chain, accounts: Map<string, Account>) {
-        let block = chain.mineBlock([
-            /* 
-             * Add transactions with: 
-             * Tx.contractCall(...)
-            */
-        ]);
-        assertEquals(block.receipts.length, 0);
-        assertEquals(block.height, 2);
+    name: "Ensure that contract initializes correctly with owner as admin",
+    async fn(chain: Chain, accounts: Map<string, Account>)
+    {
+        const deployer = accounts.get("deployer")!;
 
-        block = chain.mineBlock([
-            /* 
-             * Add transactions with: 
-             * Tx.contractCall(...)
-            */
+        let block = chain.mineBlock([
+            Tx.contractCall("proof-of-collab", "initialize", [], deployer.address),
+            Tx.contractCall("proof-of-collab", "is-project-admin",
+                [types.principal(deployer.address)],
+                deployer.address
+            )
         ]);
-        assertEquals(block.receipts.length, 0);
-        assertEquals(block.height, 3);
+
+        assertEquals(block.receipts.length, 2);
+        assertEquals(block.receipts[0].result, '(ok true)');
+        assertEquals(block.receipts[1].result, 'true');
     },
 });
+
